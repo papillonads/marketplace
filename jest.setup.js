348e56d77e79241c53b224568f742827e1cb05e8
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-17 12:06:51 +0200

    setup jest

---
+++ jest.setup.js
@@ -0,0 +1,16 @@
+import 'jest-enzyme'
+
+import renderer from 'react-test-renderer'
+
+import Enzyme, { shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+global.renderToJSON = (component) => renderer.create(component).toJSON()
+
+global.renderMount = (component) => mount(component)
+
+global.renderShallow = (component) => shallow(component)
+
+require('whatwg-fetch')
