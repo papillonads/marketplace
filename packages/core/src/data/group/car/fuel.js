880a9cdba5a489f7fafba2637fc38252c5829af0
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-25 13:01:13 +0200

    add fuels

---
+++ packages/core/src/data/group/car/fuel.js
@@ -0,0 +1,10 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const fuels = [
+  { id: uuidv4(), name: 'Benzine' },
+  { id: uuidv4(), name: 'Diesel' },
+  { id: uuidv4(), name: 'Elektrisch' },
+  { id: uuidv4(), name: 'Hybride' },
+  { id: uuidv4(), name: 'LPG' },
+  { id: uuidv4(), name: 'Other fuels' },
+]
--- packages/core/src/data/group/car/index.js
+++ packages/core/src/data/group/car/index.js
@@ -1,9 +1,11 @@
 import { brands } from './brand'
 import { maximumPrices } from './maximumPrice'
 import { bodyworks } from './bodywork'
+import { fuels } from './fuel'
 
 export const cars = {
   brands,
   maximumPrices,
   bodyworks,
+  fuels,
 }
