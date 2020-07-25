dab0fda22f1214e7e9d3079d3873d816a8df93d8
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-25 12:58:25 +0200

    add bodyworks

---
+++ packages/core/src/data/group/car/bodywork.js
@@ -0,0 +1,12 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const bodyworks = [
+  { id: uuidv4(), name: 'Hatchback' },
+  { id: uuidv4(), name: 'MPV' },
+  { id: uuidv4(), name: 'Sedan' },
+  { id: uuidv4(), name: 'Stationwagon' },
+  { id: uuidv4(), name: 'Cabriolet' },
+  { id: uuidv4(), name: 'Coupé' },
+  { id: uuidv4(), name: 'SUV of Terreinwagen' },
+  { id: uuidv4(), name: 'Other bodyworks' },
+]
--- packages/core/src/data/group/car/index.js
+++ packages/core/src/data/group/car/index.js
@@ -1,7 +1,9 @@
 import { brands } from './brand'
 import { maximumPrices } from './maximumPrice'
+import { bodyworks } from './bodywork'
 
 export const cars = {
   brands,
   maximumPrices,
+  bodyworks,
 }
