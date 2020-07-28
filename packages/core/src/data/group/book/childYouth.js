612580b0632239c92adfb527aa990eaf82bb11d8
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-28 18:10:08 +0200

    add child and youth

---
+++ packages/core/src/data/group/book/childYouth.js
@@ -0,0 +1,9 @@
+import { v4 as uuidvd } from 'uuid'
+
+export const childYouth = [
+  { id: uuidvd(), name: 'Babies and Toddlers' },
+  { id: uuidvd(), name: 'Toddlers' },
+  { id: uuidvd(), name: 'Youth | under 10 years' },
+  { id: uuidvd(), name: 'Youth | 10 to 12 years' },
+  { id: uuidvd(), name: 'Youth | 13 years and older' },
+]
--- packages/core/src/data/group/book/index.js
+++ packages/core/src/data/group/book/index.js
@@ -1,9 +1,11 @@
 import { servicesProfessionals } from './servicesProfessionals'
 import { foodCooking } from './foodCooking'
 import { historyPoliticsHistory } from './historyPoliticsHistory'
+import { childYouth } from './childYouth'
 
 export const books = {
   servicesProfessionals,
   foodCooking,
   historyPoliticsHistory,
+  childYouth,
 }
