bb89669649d855c6befe3b9a730a43d03ccbc964
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-28 18:50:35 +0200

    add cats

---
+++ packages/core/src/data/group/animalAccessory/cats.js
@@ -0,0 +1,9 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const cats = [
+  { id: uuidv4(), name: 'Studs' },
+  { id: uuidv4(), name: 'Purebred cats Short hair' },
+  { id: uuidv4(), name: 'Purebred cats Long hair' },
+  { id: uuidv4(), name: 'Other Cats' },
+  { id: uuidv4(), name: 'Accessories' },
+]
--- packages/core/src/data/group/animalAccessory/index.js
+++ packages/core/src/data/group/animalAccessory/index.js
@@ -1,9 +1,11 @@
 import { books } from './books'
 import { servicesCareBabysitterLesson } from './servicesCareBabysitterLesson'
 import { dogs } from './dogs'
+import { cats } from './cats'
 
 export const animalsAccessories = {
   books,
   servicesCareBabysitterLesson,
   dogs,
+  cats,
 }
