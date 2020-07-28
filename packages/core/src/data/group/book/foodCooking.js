64c3abe933ce57cca54ea2dd3378ae0aa5978dba
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-28 18:08:03 +0200

    alter miscellaneous names

--- packages/core/src/data/group/book/foodCooking.js
+++ packages/core/src/data/group/book/foodCooking.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid'
 
-export const foodAndCooking = [
+export const foodCooking = [
   { id: uuidv4(), name: 'Health and Nutrition' },
   { id: uuidv4(), name: 'Cookbooks' },
 ]
---
+++ packages/core/src/data/group/book/servicesProfessionals.js
@@ -0,0 +1,3 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const servicesProfessionals = [{ id: uuidv4(), name: 'Translators, Interpreters and Copywriters' }]
