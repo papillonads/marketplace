21ab80380e320b39ac7f83d7d88ccaaee95c9e02
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-28 18:11:57 +0200

    add art and culture

---
+++ packages/core/src/data/group/book/artCulture.js
@@ -0,0 +1,10 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const artCulture = [
+  { id: uuidv4(), name: 'Film, TV and Media' },
+  { id: uuidv4(), name: 'Architecture' },
+  { id: uuidv4(), name: 'Visual' },
+  { id: uuidv4(), name: 'Dance and Theater' },
+  { id: uuidv4(), name: 'Photography and Design' },
+  { id: uuidv4(), name: 'Music' },
+]
--- packages/core/src/data/group/book/index.js
+++ packages/core/src/data/group/book/index.js
@@ -2,10 +2,12 @@ import { servicesProfessionals } from './servicesProfessionals'
 import { foodCooking } from './foodCooking'
 import { historyPoliticsHistory } from './historyPoliticsHistory'
 import { childYouth } from './childYouth'
+import { artCulture } from './artCulture'
 
 export const books = {
   servicesProfessionals,
   foodCooking,
   historyPoliticsHistory,
   childYouth,
+  artCulture,
 }
