3bd4904d563c52a4db3d9d48716ddfc361d1f989
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-28 18:56:23 +0200

    add rodents and rabbits

--- packages/core/src/data/group/animalAccessory/index.js
+++ packages/core/src/data/group/animalAccessory/index.js
@@ -2,10 +2,12 @@ import { books } from './books'
 import { servicesCareBabysitterLesson } from './servicesCareBabysitterLesson'
 import { dogs } from './dogs'
 import { cats } from './cats'
+import { rodentsRabbits } from './rodentsRabbits'
 
 export const animalsAccessories = {
   books,
   servicesCareBabysitterLesson,
   dogs,
   cats,
+  rodentsRabbits,
 }
---
+++ packages/core/src/data/group/animalAccessory/rodentsRabbits.js
@@ -0,0 +1,8 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const rodentsRabbits = [
+  { id: uuidv4(), name: 'Rodents' },
+  { id: uuidv4(), name: 'Rabbits' },
+  { id: uuidv4(), name: 'Cages and Cages' },
+  { id: uuidv4(), name: 'Accessories' },
+]
