7943920e1002c09dbebcd9f6bc1d62e0443802bb
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-28 18:48:42 +0200

    add dogs

---
+++ packages/core/src/data/group/animalAccessory/dogs.js
@@ -0,0 +1,14 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const dogs = [
+  { id: uuidv4(), name: 'Beagles, Bassets and Running dogs' },
+  { id: uuidv4(), name: 'Bulldogs, Pinschers and Molossers' },
+  { id: uuidv4(), name: 'Chihuahuas and Companion Dogs' },
+  { id: uuidv4(), name: 'Sheepdogs and Cattle Drivers' },
+  { id: uuidv4(), name: 'Jack Russells and Terriers' },
+  { id: uuidv4(), name: 'Polar dogs, Keeshonden and Primal types' },
+  { id: uuidv4(), name: 'Retrievers, Spaniels and Water Dogs' },
+  { id: uuidv4(), name: 'Setters and Standing dogs' },
+  { id: uuidv4(), name: 'Dachshunds and Dachshunds' },
+  { id: uuidv4(), name: 'Greyhounds' },
+]
--- packages/core/src/data/group/animalAccessory/index.js
+++ packages/core/src/data/group/animalAccessory/index.js
@@ -1,7 +1,9 @@
 import { books } from './books'
 import { servicesCareBabysitterLesson } from './servicesCareBabysitterLesson'
+import { dogs } from './dogs'
 
 export const animalsAccessories = {
   books,
   servicesCareBabysitterLesson,
+  dogs,
 }
