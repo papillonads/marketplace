b0b89a5a86c70ab4bfff1781eab12199a77bb94c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-28 18:03:19 +0200

    add food and cooking

---
+++ packages/core/src/data/group/book/foodAndCooking.js
@@ -0,0 +1,6 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const foodAndCooking = [
+  { id: uuidv4(), name: 'Health and Nutrition' },
+  { id: uuidv4(), name: 'Cookbooks' },
+]
--- packages/core/src/data/group/book/index.js
+++ packages/core/src/data/group/book/index.js
@@ -1,5 +1,7 @@
 import { servicesAndProfessionals } from './servicesAndProfessionals'
+import { foodAndCooking } from './foodAndCooking'
 
 export const books = {
   servicesAndProfessionals,
+  foodAndCooking,
 }
