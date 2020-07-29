3e49b6fb8c21a4dd125fe9b44f2b21cf0862998b
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-29 22:23:07 +0200

    extend data group index with audio, tv and photo

--- packages/core/src/data/group/index.js
+++ packages/core/src/data/group/index.js
@@ -2,5 +2,6 @@ import { cars } from './car'
 import { books } from './book'
 import { animalsAccessories } from './animalAccessory'
 import { antiqueArt } from './antiqueArt'
+import { audioTVPhoto } from './audioTVPhoto'
 
-export const groups = [cars, books, animalsAccessories, antiqueArt]
+export const groups = [cars, books, animalsAccessories, antiqueArt, audioTVPhoto]
