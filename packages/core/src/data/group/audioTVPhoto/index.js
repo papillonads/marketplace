2a133deefc2eac873ebd469e5a17108bdc38ffd2
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-29 22:28:05 +0200

    add services and professionals

--- packages/core/src/data/group/audioTVPhoto/index.js
+++ packages/core/src/data/group/audioTVPhoto/index.js
@@ -1,9 +1,11 @@
 import { accessories } from './accessories'
 import { audio } from './audio'
 import { audioPortable } from './audioPortable'
+import { servicesProfessionals } from './servicesProfessionals'
 
 export const audioTVPhoto = {
   accessories,
   audio,
   audioPortable,
+  servicesProfessionals,
 }
---
+++ packages/core/src/data/group/audioTVPhoto/servicesProfessionals.js
@@ -0,0 +1,7 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const servicesProfessionals = [
+  { id: uuidv4(), name: 'Film and Video Editing' },
+  { id: uuidv4(), name: 'Photographers' },
+  { id: uuidv4(), name: 'Repairs' },
+]
