187a3cd8aa6466338f32c33a3a913df93989a055
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-29 22:12:18 +0200

    add services and professionals

--- packages/core/src/data/group/antiqueArt/index.js
+++ packages/core/src/data/group/antiqueArt/index.js
@@ -2,10 +2,12 @@ import { antiquesEatingUtensils } from './antiquesEatingUtensils'
 import { antiquesUtensils } from './antiquesUtensils'
 import { antiquesFurniture } from './antiquesFurniture'
 import { curiositiesBrocante } from './curiositiesBrocante'
+import { servicesProfessionals } from './servicesProfessionals'
 
 export const antiqueArt = {
   antiquesEatingUtensils,
   antiquesUtensils,
   antiquesFurniture,
   curiositiesBrocante,
+  servicesProfessionals,
 }
---
+++ packages/core/src/data/group/antiqueArt/servicesProfessionals.js
@@ -0,0 +1,9 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const servicesProfessionals = [
+  { id: uuidv4(), name: 'Goldsmiths and Jewelers' },
+  { id: uuidv4(), name: 'Photographers' },
+  { id: uuidv4(), name: 'Artists and Portrait Painters' },
+  { id: uuidv4(), name: 'Repair and maintenance' },
+  { id: uuidv4(), name: 'Carpenters and Furniture Makers' },
+]
