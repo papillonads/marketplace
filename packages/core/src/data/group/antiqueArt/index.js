38226f941f08b48cd66a583cc89958ffa11856ee
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-29 22:10:51 +0200

    add curiosities and brocante

--- packages/core/src/data/group/antiqueArt/index.js
+++ packages/core/src/data/group/antiqueArt/index.js
@@ -1,9 +1,11 @@
 import { antiquesEatingUtensils } from './antiquesEatingUtensils'
 import { antiquesUtensils } from './antiquesUtensils'
 import { antiquesFurniture } from './antiquesFurniture'
+import { curiositiesBrocante } from './curiositiesBrocante'
 
 export const antiqueArt = {
   antiquesEatingUtensils,
   antiquesUtensils,
   antiquesFurniture,
+  curiositiesBrocante,
 }
