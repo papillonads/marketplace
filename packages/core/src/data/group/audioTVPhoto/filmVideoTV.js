076842cdd633c7aa7bc8019d65946d6353497b2d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-29 22:31:03 +0200

    add film, video and tv

---
+++ packages/core/src/data/group/audioTVPhoto/filmVideoTV.js
@@ -0,0 +1,20 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const filmVideoTV = [
+  { id: uuidv4(), name: 'Beamers and Projection Equipment' },
+  { id: uuidv4(), name: 'Blu-ray players and recorders' },
+  { id: uuidv4(), name: 'DVD players and DVD recorders' },
+  { id: uuidv4(), name: 'Films 8mm, 16mm and 35mm' },
+  { id: uuidv4(), name: 'Hard disc recorders' },
+  { id: uuidv4(), name: 'Home cinema sets' },
+  { id: uuidv4(), name: 'Media players' },
+  { id: uuidv4(), name: 'Sports and Action Cameras' },
+  { id: uuidv4(), name: 'TV decoders and Satellite dishes' },
+  { id: uuidv4(), name: 'TVs | Plasma, Lcd, Led' },
+  { id: uuidv4(), name: 'TVs | Other TVs' },
+  { id: uuidv4(), name: 'TVs | accessories' },
+  { id: uuidv4(), name: 'Video surveillance and Doorbells' },
+  { id: uuidv4(), name: 'Video cameras Analog' },
+  { id: uuidv4(), name: 'Video cameras Digital' },
+  { id: uuidv4(), name: 'Video players and Video recorders' },
+]
--- packages/core/src/data/group/audioTVPhoto/index.js
+++ packages/core/src/data/group/audioTVPhoto/index.js
@@ -2,10 +2,12 @@ import { accessories } from './accessories'
 import { audio } from './audio'
 import { audioPortable } from './audioPortable'
 import { servicesProfessionals } from './servicesProfessionals'
+import { filmVideoTV } from './filmVideoTV'
 
 export const audioTVPhoto = {
   accessories,
   audio,
   audioPortable,
   servicesProfessionals,
+  filmVideoTV,
 }
