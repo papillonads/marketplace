900b871cb228c8e3d8b8592a444674b979e539d5
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-29 22:59:58 +0200

    extend data group index with car miscellaneous

--- packages/core/src/data/group/index.js
+++ packages/core/src/data/group/index.js
@@ -4,5 +4,6 @@ import { animalsAccessories } from './animalAccessory'
 import { antiqueArt } from './antiqueArt'
 import { audioTVPhoto } from './audioTVPhoto'
 import { carParts } from './carParts'
+import { carMiscellaneous } from './carMiscellaneous'
 
-export const groups = [cars, books, animalsAccessories, antiqueArt, audioTVPhoto, carParts]
+export const groups = [cars, books, animalsAccessories, antiqueArt, audioTVPhoto, carParts, carMiscellaneous]
