f0c7407b1cfc79efbb2f110f27d0a7dacae014bc
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-30 23:11:33 +0200

    add purchase

--- packages/core/src/data/group/caravansCamping/index.js
+++ packages/core/src/data/group/caravansCamping/index.js
@@ -1,9 +1,11 @@
 import { accessoriesAccessories } from './accessoriesAccessories'
 import { caravansCampers } from './caravansCampers'
 import { services } from './services'
+import { purchase } from './purchase'
 
 export const caravansCamping = {
   accessoriesAccessories,
   caravansCampers,
   services,
+  purchase,
 }
---
+++ packages/core/src/data/group/caravansCamping/purchase.js
@@ -0,0 +1,6 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const purchase = [
+  { id: uuidv4(), name: 'Camper' },
+  { id: uuidv4(), name: 'Caravan' },
+]
