5557208810c583ad6f6e5a1b7245fa6e411a5418
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-30 22:59:23 +0200

    add services and professionals

--- packages/core/src/data/group/carMiscellaneous/index.js
+++ packages/core/src/data/group/carMiscellaneous/index.js
@@ -2,10 +2,12 @@ import { trailers } from './trailers'
 import { accessories } from './accessories'
 import { cars } from './cars'
 import { booksManuals } from './booksManuals'
+import { servicesProfessionals } from './servicesProfessionals'
 
 export const carMiscellaneous = {
   trailers,
   accessories,
   cars,
   booksManuals,
+  servicesProfessionals,
 }
---
+++ packages/core/src/data/group/carMiscellaneous/servicesProfessionals.js
@@ -0,0 +1,9 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const servicesProfessionals = [
+  { id: uuidv4(), name: 'Automonteurs en Garages' },
+  { id: uuidv4(), name: 'Autopoetsers en Autowassers' },
+  { id: uuidv4(), name: 'Autostallingen en Garages' },
+  { id: uuidv4(), name: 'Autoverhuur' },
+  { id: uuidv4(), name: 'Schadeherstellers en Spuiterijen' },
+]
