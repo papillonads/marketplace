9b73c2f0161086cfc69d0f6b211e3a295513aba0
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-30 23:12:51 +0200

    add tents and trailer tents

--- packages/core/src/data/group/caravansCamping/index.js
+++ packages/core/src/data/group/caravansCamping/index.js
@@ -2,10 +2,12 @@ import { accessoriesAccessories } from './accessoriesAccessories'
 import { caravansCampers } from './caravansCampers'
 import { services } from './services'
 import { purchase } from './purchase'
+import { tentsTrailerTents } from './tentsTrailerTents'
 
 export const caravansCamping = {
   accessoriesAccessories,
   caravansCampers,
   services,
   purchase,
+  tentsTrailerTents,
 }
---
+++ packages/core/src/data/group/caravansCamping/tentsTrailerTents.js
@@ -0,0 +1,7 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const tentsTrailerTents = [
+  { id: uuidv4(), name: 'Bungalow tents' },
+  { id: uuidv4(), name: 'Tents' },
+  { id: uuidv4(), name: 'Folding trailers' },
+]
