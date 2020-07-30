614ff7438c703203f1eb36241da620d81739c9ba
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-30 22:58:18 +0200

    add books and manuals

---
+++ packages/core/src/data/group/carMiscellaneous/booksManuals.js
@@ -0,0 +1,7 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const booksManuals = [
+  { id: uuidv4(), name: 'Car books' },
+  { id: uuidv4(), name: 'Leaflets and Magazines' },
+  { id: uuidv4(), name: 'Manuals and Instruction Booklets' },
+]
--- packages/core/src/data/group/carMiscellaneous/index.js
+++ packages/core/src/data/group/carMiscellaneous/index.js
@@ -1,9 +1,11 @@
 import { trailers } from './trailers'
 import { accessories } from './accessories'
 import { cars } from './cars'
+import { booksManuals } from './booksManuals'
 
 export const carMiscellaneous = {
   trailers,
   accessories,
   cars,
+  booksManuals,
 }
