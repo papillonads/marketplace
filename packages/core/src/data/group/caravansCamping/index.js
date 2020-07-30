a1aac6aa0b2e9a93141a338655317bafa5e60b62
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-30 23:10:38 +0200

    add services

--- packages/core/src/data/group/caravansCamping/index.js
+++ packages/core/src/data/group/caravansCamping/index.js
@@ -1,7 +1,9 @@
 import { accessoriesAccessories } from './accessoriesAccessories'
 import { caravansCampers } from './caravansCampers'
+import { services } from './services'
 
 export const caravansCamping = {
   accessoriesAccessories,
   caravansCampers,
+  services,
 }
---
+++ packages/core/src/data/group/caravansCamping/services.js
@@ -0,0 +1,8 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const services = [
+  { id: uuidv4(), name: 'Caravan centers' },
+  { id: uuidv4(), name: 'Caravan sheds' },
+  { id: uuidv4(), name: 'Repair and maintenance' },
+  { id: uuidv4(), name: 'Rental' },
+]
