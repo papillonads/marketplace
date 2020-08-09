724d2597b620a77d216410de143ac4394032ff7e
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-08-09 12:06:22 +0200

    add vinyl

--- packages/core/src/data/group/CDsDVDs/index.js
+++ packages/core/src/data/group/CDsDVDs/index.js
@@ -2,10 +2,12 @@ import { bluRays } from './bluRays'
 import { cds } from './cds'
 import { dvds } from './dvds'
 import { vhs } from './vhs'
+import { vinyl } from './vinyl'
 
 export const CDsDVDs = {
   bluRays,
   cds,
   dvds,
   vhs,
+  vinyl,
 }
---
+++ packages/core/src/data/group/CDsDVDs/vinyl.js
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const vinyl = [
+  { id: uuidv4(), name: 'Singles' },
+  { id: uuidv4(), name: 'Country and Western' },
+  { id: uuidv4(), name: 'Dance and House' },
+  { id: uuidv4(), name: 'Film scores and Soundtracks' },
+  { id: uuidv4(), name: 'Hard rock and Metal' },
+  { id: uuidv4(), name: 'Hip Hop and Rap' },
+  { id: uuidv4(), name: 'Jazz and Blues' },
+  { id: uuidv4(), name: 'Children and Youth' },
+  { id: uuidv4(), name: 'Classic' },
+  { id: uuidv4(), name: 'Latin and Salsa' },
+  { id: uuidv4(), name: 'Dutch speaking' },
+  { id: uuidv4(), name: 'Pop' },
+  { id: uuidv4(), name: 'R&B and Soul' },
+  { id: uuidv4(), name: 'Rock' },
+  { id: uuidv4(), name: 'Collection albums' },
+  { id: uuidv4(), name: 'World music' },
+  { id: uuidv4(), name: 'Others' },
+]
