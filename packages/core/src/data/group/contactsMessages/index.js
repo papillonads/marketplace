77fb1f53b894a69e3052d14f72108153ef84f7be
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-08-09 12:40:14 +0200

    add calls

--- packages/core/src/data/group/contactsMessages/index.js
+++ packages/core/src/data/group/contactsMessages/index.js
@@ -1,9 +1,11 @@
 import { datingNightlifeRelationships } from './datingNightlifeRelationships'
 import { servicesProfessionals } from './servicesProfessionals'
 import { partiesEvents } from './partiesEvents'
+import { wantedCalls } from './wantedCalls'
 
 export const contactsMessages = {
   datingNightlifeRelationships,
   servicesProfessionals,
   partiesEvents,
+  wantedCalls,
 }
---
+++ packages/core/src/data/group/contactsMessages/wantedCalls.js
@@ -0,0 +1,9 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const wantedCalls = [
+  { id: uuidv4(), name: 'Advice and Calls' },
+  { id: uuidv4(), name: 'Housemates' },
+  { id: uuidv4(), name: 'Music and Band members' },
+  { id: uuidv4(), name: 'Travel companions' },
+  { id: uuidv4(), name: 'Sport and Hobby' },
+]
