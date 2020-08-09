352f5a10ed7f3ae779e2dd947c1ce06b85493b49
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-08-09 12:45:34 +0200

    add detection

---
+++ packages/core/src/data/group/contactsMessages/detection.js
@@ -0,0 +1,8 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const detection = [
+  { id: uuidv4(), name: 'Contact and Investigation' },
+  { id: uuidv4(), name: 'Found objects' },
+  { id: uuidv4(), name: 'Reunions' },
+  { id: uuidv4(), name: 'Missing animals' },
+]
--- packages/core/src/data/group/contactsMessages/index.js
+++ packages/core/src/data/group/contactsMessages/index.js
@@ -2,10 +2,12 @@ import { datingNightlifeRelationships } from './datingNightlifeRelationships'
 import { servicesProfessionals } from './servicesProfessionals'
 import { partiesEvents } from './partiesEvents'
 import { wantedCalls } from './wantedCalls'
+import { detection } from './detection'
 
 export const contactsMessages = {
   datingNightlifeRelationships,
   servicesProfessionals,
   partiesEvents,
   wantedCalls,
+  detection,
 }
