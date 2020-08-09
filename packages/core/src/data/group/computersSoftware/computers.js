da2edea050a05969331867ca3149cd8160ee9981
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-08-09 12:16:12 +0200

    add computers

---
+++ packages/core/src/data/group/computersSoftware/computers.js
@@ -0,0 +1,9 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const computers = [
+  { id: uuidv4(), name: 'Apple Desktops' },
+  { id: uuidv4(), name: 'Commodore' },
+  { id: uuidv4(), name: 'Desktop PCs' },
+  { id: uuidv4(), name: 'Servers' },
+  { id: uuidv4(), name: 'Sun, Sgi and Unix' },
+]
--- packages/core/src/data/group/computersSoftware/index.js
+++ packages/core/src/data/group/computersSoftware/index.js
@@ -1,7 +1,9 @@
 import { books } from './books'
 import { computerParts } from './computerParts'
+import { computers } from './computers'
 
 export const computersSoftware = {
   books,
   computerParts,
+  computers,
 }
