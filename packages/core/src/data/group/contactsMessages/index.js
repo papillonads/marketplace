53d61b1c0699762e1f3c4d6a5e7ec0671a28098e
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-08-09 12:39:19 +0200

    add events

--- packages/core/src/data/group/contactsMessages/index.js
+++ packages/core/src/data/group/contactsMessages/index.js
@@ -1,7 +1,9 @@
 import { datingNightlifeRelationships } from './datingNightlifeRelationships'
 import { servicesProfessionals } from './servicesProfessionals'
+import { partiesEvents } from './partiesEvents'
 
 export const contactsMessages = {
   datingNightlifeRelationships,
   servicesProfessionals,
+  partiesEvents,
 }
---
+++ packages/core/src/data/group/contactsMessages/partiesEvents.js
@@ -0,0 +1,6 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const partiesEvents = [
+  { id: uuidv4(), name: 'Events' },
+  { id: uuidv4(), name: 'Homeparties' },
+]
