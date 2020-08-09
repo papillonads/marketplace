a4392d26d9847bb751ced0b31e23a580e7972d88
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-08-09 12:19:43 +0200

    laptops

--- packages/core/src/data/group/computersSoftware/index.js
+++ packages/core/src/data/group/computersSoftware/index.js
@@ -2,10 +2,12 @@ import { books } from './books'
 import { computerParts } from './computerParts'
 import { computers } from './computers'
 import { servicesProfessionals } from './servicesProfessionals'
+import { laptops } from './laptops'
 
 export const computersSoftware = {
   books,
   computerParts,
   computers,
   servicesProfessionals,
+  laptops,
 }
---
+++ packages/core/src/data/group/computersSoftware/laptops.js
@@ -0,0 +1,10 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const laptops = [
+  { id: uuidv4(), name: 'Batteries and Batteries' },
+  { id: uuidv4(), name: 'Apple' },
+  { id: uuidv4(), name: 'Laptops and Notebooks' },
+  { id: uuidv4(), name: 'Mini Laptops and Netbooks' },
+  { id: uuidv4(), name: 'Parts, Accessories' },
+  { id: uuidv4(), name: 'Bags and Covers' },
+]
