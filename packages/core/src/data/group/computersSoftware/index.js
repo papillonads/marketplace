4cd8b08512acaf58e33aa3d1c70319197dc4231d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-08-09 12:18:28 +0200

    add services and professionals

--- packages/core/src/data/group/computersSoftware/index.js
+++ packages/core/src/data/group/computersSoftware/index.js
@@ -1,9 +1,11 @@
 import { books } from './books'
 import { computerParts } from './computerParts'
 import { computers } from './computers'
+import { servicesProfessionals } from './servicesProfessionals'
 
 export const computersSoftware = {
   books,
   computerParts,
   computers,
+  servicesProfessionals,
 }
---
+++ packages/core/src/data/group/computersSoftware/servicesProfessionals.js
@@ -0,0 +1,7 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const servicesProfessionals = [
+  { id: uuidv4(), name: 'Computer and Internet experts' },
+  { id: uuidv4(), name: 'Repair and maintenance' },
+  { id: uuidv4(), name: 'Web designers and Hosting' },
+]
