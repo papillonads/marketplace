4168ecbbdb96490846f859840806c38b301df21b
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-08-09 12:03:45 +0200

    add vhs

--- packages/core/src/data/group/CDsDVDs/index.js
+++ packages/core/src/data/group/CDsDVDs/index.js
@@ -1,9 +1,11 @@
 import { bluRays } from './bluRays'
 import { cds } from './cds'
 import { dvds } from './dvds'
+import { vhs } from './vhs'
 
 export const CDsDVDs = {
   bluRays,
   cds,
   dvds,
+  vhs,
 }
---
+++ packages/core/src/data/group/CDsDVDs/vhs.js
@@ -0,0 +1,7 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const vhs = [
+  { id: uuidv4(), name: 'Movie' },
+  { id: uuidv4(), name: 'Documentary, TV and Music' },
+  { id: uuidv4(), name: 'Children and Youth' },
+]
